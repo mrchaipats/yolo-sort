7f5dfa0ca0b9dd7ca68f3f9e9e5e028880fd3736
mrchaipats <mrchaipats@gmail.com>
2021-09-10 18:27:39 +0700

    implement KalmanBoxTracker and utility functions

--- kalman.py
+++ kalman.py
@@ -1,14 +1,77 @@
+import numpy as np
+from filterpy.kalman import KalmanFilter
+
+from utils import convert_x_to_bbox, convert_bbox_to_z
+
+
 class KalmanBoxTracker:
     count = 0
 
     def __init__(self, bbox):
-        pass
+        # define linear constant velocity model
+        self.kf = KalmanFilter(dim_x=7, dim_z=4)
+        self.kf.F = np.array([
+            [1, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 1, 0, 0, 0, 1],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+        ])
+        self.kf.H = np.array([
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+        ])
+
+        self.kf.R[2:, 2:] *= 10.
+        self.kf.P[4:, 4:] *= 1000.  # give high uncertainty to the unobservable initial velocities
+        self.kf.P *= 10.
+        self.kf.Q[-1, -1] *= 0.01
+        self.kf.Q[4:, 4:] *= 0.01
+
+        self.kf.x[:4] = convert_bbox_to_z(bbox)
+        self.time_since_update = 0
+        self.id = KalmanBoxTracker.count
+        KalmanBoxTracker.count += 1
+        self.history = []
+        self.hits = 0
+        self.hit_streak = 0
+        self.age = 0
 
     def update(self, bbox):
-        pass
+        """
+        Updates the state vector with observed bbox.
+        :param bbox:
+        :return:
+        """
+        self.time_since_update = 0
+        self.history = []
+        self.hits += 1
+        self.hit_streak += 1
+        self.kf.update(convert_bbox_to_z(bbox))
 
     def predict(self):
-        pass
+        """
+        Advances the state vector and returns the predicted bounding box estimate.
+        :return:
+        """
+        # box scale (area) <= 0 doesn't make sense, so retain the previous box area
+        if self.kf.x[6] + self.kf.x[2] <= 0:
+            self.kf.x[6] *= 0.0
+        self.kf.predict()
+        self.age += 1
+        if self.time_since_update > 0:
+            self.hit_streak = 0
+        self.time_since_update += 1
+        self.history.append(convert_x_to_bbox(self.kf.x))
+        return self.history[-1]
 
     def get_state(self):
-        pass
+        """
+        Returns the current bounding box estimate.
+        :return:
+        """
+        return convert_x_to_bbox(self.kf.x)
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 
@@ -8,3 +9,47 @@ def linear_assignment(cost_matrix):
 
 def associate_detections_to_trackers(detections, trackers, iou_threshold=0.3):
     pass
+
+
+def convert_bbox_to_z(bbox):
+    """
+    Convert a bounding box in the form [x1, y1, x2, y2] and returns z in the form
+    [x, y, s, r] where x, y is the center of the box and s is the scale/area and r
+    is the aspect ratio
+    :param bbox: [x1, y1, x2, y2]
+    :return: [x, y, s, r]
+    """
+    w = bbox[2] - bbox[0]
+    h = bbox[3] - bbox[1]
+    x = bbox[0] + w / 2.
+    y = bbox[1] + h / 2.
+    s = w * h
+    r = w / float(h)
+    return np.array([x, y, s, r]).reshape((4, 1))
+
+
+def convert_x_to_bbox(x, score=None):
+    """
+    Convert a bounding box in the center form [x, y, s, r] to itself in the form
+    [x1, y1, x2, y2] where (x1, y1) is the top left and (x2, y2) is the bottom right
+    :param x: [x, y, s, r]
+    :param score: Optional
+    :return: [x1, y1, x2, y2] if score is None else [x1, y1, x2, y2, score]
+    """
+    w = np.sqrt(x[2] * x[3])
+    h = x[2] / w
+    if score is None:
+        return np.array([
+            x[0] - w / 2.,
+            x[1] - h / 2.,
+            x[0] + w / 2.,
+            x[1] + h / 2.,
+        ])
+    else:
+        return np.array([
+            x[0] - w / 2.,
+            x[1] - h / 2.,
+            x[0] + w / 2.,
+            x[1] + h / 2.,
+            score,
+        ])
