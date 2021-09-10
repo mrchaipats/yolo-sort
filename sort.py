97940b75c7e51dc97f778286adb9ff52c95702b0
mrchaipats <mrchaipats@gmail.com>
2021-09-10 17:01:48 +0700

    add skeleton for SORT class

---
+++ sort.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Sort:
+    def __init__(self, max_age=1, min_hits=3, iou_threshold=0.3):
+        self.max_age = max_age
+        self.min_hits = min_hits
+        self.iou_threshold = iou_threshold
+        self.trackers = []
+        self.frame_count = 0
+
+    def update(self, detections=np.empty((0, 5))):
+        pass
